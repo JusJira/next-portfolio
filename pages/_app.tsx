34fead7522aa351379cd1864ae2bc6f39142a41b
JustinJira <jus.jira@gmail.com>
2023-02-02 16:22:27 +0700

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <ThemeProvider enableSystem={true} attribute="class">
+      <ThemeProvider attribute="class">
         <Component {...pageProps} />
       </ThemeProvider>)
 }
