3bae03ff850463ea09cb0dfc9c0627065355dfe5
JustinJira <jus.jira@gmail.com>
2023-02-02 10:48:24 +0700

    feat: Image Optimization

--- components/CertBox.tsx
+++ components/CertBox.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 function CertBox(props: { certDetail: { name: any; thumbnail: any; source: any; date: any; }; }) {
     const { name, thumbnail, source, date } = props.certDetail;
@@ -6,10 +7,12 @@ function CertBox(props: { certDetail: { name: any; thumbnail: any; source: any; 
         <article className="w-full max-h-[25rem] overflow-hidden relative group">
             <div className="flex items-center absolute right-4 top-4 z-10">
             </div>
-            <img
+            <Image
                 src={thumbnail}
-                className="w-full h-auto scale-100 hover:scale-110 transition duration-[1.5s]"
-                alt="featureImage"
+                className="object-cover w-full h-auto aspect-video scale-100 hover:scale-110 transition duration-[1.5s]"
+                alt="certImage"
+                width={1000}
+                height={1000}
             />
             <div
                 className="absolute w-full bottom-0 left-0 backdrop-blur-md dark:text-white text-white bg-black/50 p-4 translate-y-[100%] group-hover:translate-y-0 transition duration-700"
