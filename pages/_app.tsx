a99f5ff25bb3b289115f5237c0e059cb227aa909
JustinJira <jus.jira@gmail.com>
2023-02-02 15:40:48 +0700

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import '../styles/globals.css'
 import '../styles/font.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
+import React from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <ThemeProvider enableSystem={true} attribute="class">
-    <Component {...pageProps} />
-  </ThemeProvider>)
+    <React.StrictMode>
+      <ThemeProvider enableSystem={true} attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.StrictMode>)
 }
 
 export default MyApp
