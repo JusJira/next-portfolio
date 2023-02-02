6a8783f532dce891ddac11575f31f07f14390748
JustinJira <jus.jira@gmail.com>
2023-02-02 16:41:15 +0700

    style: pre allocate space for theme changer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,13 +13,13 @@ function Navbar() {
   },[])
 
   const renderThemeChanger = () => {
-    if(!mounted) return null;
+    if(!mounted) return (<div className='w-[20px] h-[20px]'/>);
     
     const currentTheme = theme === "system" ? systemTheme : theme;
 
     if (currentTheme === "dark") {
       return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-sun text-white"
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-sun text-white"
         onClick={() => setTheme('light')}>
             <circle cx="12" cy="12" r="5"></circle>
             <line x1="12" y1="1" x2="12" y2="3"></line>
@@ -36,7 +36,7 @@ function Navbar() {
 
     else {
       return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-moon"
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-moon"
         onClick={() => setTheme('dark')}>
           <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
         </svg>
